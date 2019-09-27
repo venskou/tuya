6a1cc1de701d4d66c882e5b2e9f9b8421fe748d1
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-09-27 18:27:14 +0300

    Nav logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 svg4everybody();
+
+const navToggle = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const mobileQuery = window.matchMedia('(max-width: 767.98px)');
+let isNavOpen = false;
+let isMobile = false;
+
+function toggleNavVisibility() {
+  navToggle.classList.toggle('nav__toggle--active');
+  nav.classList.toggle('nav--active');
+  isNavOpen = !isNavOpen;
+}
+
+function screenTest(query) {
+  isMobile = query.matches;
+  if (!isMobile && isNavOpen) {
+    toggleNavVisibility();
+  }
+}
+
+navToggle.addEventListener('click', toggleNavVisibility);
+mobileQuery.addListener(screenTest);
+document.addEventListener('DOMContentLoaded', screenTest);
